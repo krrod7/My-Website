c561cfbb9747886a32a218e33747ec4fdf55825b
krrod7 <krrod7@bu.edu>
2024-05-28 22:28:15 -0400

    added a word

--- pages/cocktailmania.js
+++ pages/cocktailmania.js
@@ -35,7 +35,7 @@ function Cocktailmania() {
         <h2>Designed and developed a web application that allows users to search for cocktails. Results of search include detailed step-by-step recipe instructions with a list of ingredients and an image of the drink. </h2>
         <br/>
         <h1><span className={styles.label}>DEV TOOLS</span></h1>
-        <h2>Angular (frontend) and Express (Node.js backend), coded on WebStorm </h2>
+        <h2>Angular (frontend), Express (Node.js backend), Redis, coded on WebStorm </h2>
         <br/>
         <h1><span className={styles.label}>APIS</span></h1>
         <h2>The Cocktail DB API</h2>
